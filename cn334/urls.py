fbf48ec45a5e5c3827199a19ef2f4577c2bd3773
charatphan <98724111+charatphan@users.noreply.github.com>
2024-01-28 09:05:16 +0000

    Adding new ecommerce app new page

--- cn334/urls.py
+++ cn334/urls.py
@@ -22,5 +22,11 @@ from ecommerce import views as ecom_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("ecommerce/", ecom_views.ecommerce_index_view),
-    path("ecommerce/item/<item_id>", ecom_views.item_view)
+    path("ecommerce/item/<item_id>", ecom_views.item_view),
+    path("ecommerce/homepage/<home_id>", ecom_views.home_view),
+    path("ecommerce/category/<category_id>", ecom_views.category_view),
+    path("ecommerce/product/<product_id>", ecom_views.product_view),
+    path("ecommerce/checkout/<checkout_id>", ecom_views.checkout_view),
+    path("ecommerce/contact/<contact_id>", ecom_views.contact_view)
 ]
+
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -11,3 +11,33 @@ def item_view(request, item_id):
         "item_id": item_id
        }
     return render(request, 'index.html',context= context_data)
+
+def home_view(request, home_id):
+    context_data = {
+        "home_id": home_id
+       }
+    return render(request, 'home.html',context= context_data)
+
+def category_view(request, category_id):
+    context_data = {
+        "category_id": category_id
+       }
+    return render(request, 'category.html',context= context_data)
+
+def product_view(request, product_id):
+    context_data = {
+        "product_id": product_id
+       }
+    return render(request, 'product.html',context= context_data)
+
+def checkout_view(request, checkout_id):
+    context_data = {
+        "checkout_id": checkout_id
+       }
+    return render(request, 'checkout.html',context= context_data)
+
+def contact_view(request, contact_id):
+    context_data = {
+        "checkout_id": contact_id
+       }
+    return render(request, 'contact.html',context= context_data)
